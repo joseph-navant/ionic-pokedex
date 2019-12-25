a7cbaaf196d53a5a11e00b5e5dfe138f2b8f9ef9
Joseph <joseph.morantnavarro@gmail.com>
2019-12-25 16:01:59 +0100

    Working on pokemon list page

--- src/app/pages/pokemon-list/pokemon-list.page.ts
+++ src/app/pages/pokemon-list/pokemon-list.page.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { Pokemon } from 'src/app/models/pokeapi';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.page.html',
-  styleUrls: ['./pokemon-list.page.scss'],
+  styleUrls: ['./pokemon-list.page.scss']
 })
 export class PokemonListPage implements OnInit {
+  private offset = 0;
+  pokemons: Pokemon[] = [];
 
-  constructor() { }
+  constructor(private pokemonService: PokemonService) {}
 
   ngOnInit() {
+    this.loadPokemon();
   }
 
+  loadPokemon(loadMore = false, evt?: { target: { complete: () => void } }) {
+    if (loadMore) {
+      this.offset += 25;
+    }
+
+    this.pokemonService
+      .getPokemonList(this.offset)
+      .pipe(take(1))
+      .subscribe((pokemons: Pokemon[]) => {
+        console.log(this.pokemons.length);
+        this.pokemons = [...this.pokemons, ...pokemons];
+        console.log(this.pokemons.length);
+
+        if (evt) {
+          evt.target.complete();
+        }
+      });
+  }
+
+  onSearch(evt: CustomEvent) {
+    const value = evt.detail.value;
+
+    if (value) {
+      this.pokemonService.findPokemon(value).subscribe(
+        res => {
+          this.pokemons = [res];
+        },
+        err => {
+          this.pokemons = [];
+          console.error(err);
+        }
+      );
+    } else {
+      this.offset = 0;
+      this.pokemons = [];
+      this.loadPokemon();
+      return;
+    }
+  }
 }

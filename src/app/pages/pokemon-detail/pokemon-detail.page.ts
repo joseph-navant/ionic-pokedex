3247d99030bf2f8ebe7a1a5eb3f52391d4573efa
Joseph <joseph.morantnavarro@gmail.com>
2019-12-25 16:09:40 +0100

    Working on pokemon detail page

--- src/app/pages/pokemon-detail/pokemon-detail.page.ts
+++ src/app/pages/pokemon-detail/pokemon-detail.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { Pokemon } from 'src/app/models/pokeapi';
 
 @Component({
   selector: 'app-pokemon-detail',
   templateUrl: './pokemon-detail.page.html',
-  styleUrls: ['./pokemon-detail.page.scss'],
+  styleUrls: ['./pokemon-detail.page.scss']
 })
 export class PokemonDetailPage implements OnInit {
+  pokemon: Pokemon;
+  slideOpts = {
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false
+    }
+  };
 
-  constructor() { }
+  constructor(private pokemonService: PokemonService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.pokemonService.getPokemonDetails(id).subscribe(pokemon => {
+      this.pokemon = pokemon;
+    });
   }
-
 }
